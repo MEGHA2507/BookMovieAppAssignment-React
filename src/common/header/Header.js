449b2647f067d3b8a78843d52f28d4381b4eebbf
MEGHA2507 <megha.banerjee07@gmail.com>
2022-02-09 18:59:44 +0530

    Header cmp changes pushed

---
+++ src/common/header/Header.js
@@ -0,0 +1,295 @@
+import React, { Fragment } from "react";
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import Button from "@material-ui/core/Button";
+import Modal from "react-modal";
+import { useState } from "react";
+
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+//import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import { ValidatorForm } from "react-material-ui-form-validator";
+import TextValidator from "react-material-ui-form-validator/lib/TextValidator";
+
+import { FormControl, InputLabel, Input } from "@material-ui/core";
+
+Modal.setAppElement("#root");
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={2}>
+          <Typography component={"div"}>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    textAlign: "center",
+  },
+}));
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+export default function Header() {
+  let loggedIn = true; // later this variable will be dependent on access token
+  let releaseMovie = true;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
+
+  const [loginForm, setloginForm] = useState([
+    {
+      id: 0,
+      username: "",
+      password: "",
+    },
+  ]);
+
+  function inputChangeHandler(e) {
+    const state = loginForm;
+    state[e.target.name] = e.target.value;
+
+    setloginForm({ ...loginForm, ...state });
+  }
+
+  function loginHandler() {
+    // authenticate hoga
+
+    setloginForm({
+      id: 0,
+      username: "",
+      password: "",
+    });
+    setIsOpen(false);
+  }
+
+  function registerHandler() {
+    setIsOpen(false);
+  }
+
+  function bookShowBtnHandler() {
+    if (loggedIn === true) {
+      setIsOpen(true);
+    } else {
+      console.log("route to book show page");
+    }
+  }
+
+  const { username, password } = loginForm;
+
+  return (
+    <Fragment>
+      <div className="header-container">
+        <img className="logo-img" src={logo} alt="logo"></img>
+        <div className="header-btn-container">
+          {releaseMovie ? (
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={bookShowBtnHandler}
+            >
+              Book Show
+            </Button>
+          ) : (
+            ""
+          )}
+          {loggedIn ? (
+            <Button variant="contained" onClick={toggleModal}>
+              Login
+            </Button>
+          ) : (
+            <Button variant="contained">Logout</Button>
+          )}
+          <Modal
+            isOpen={isOpen}
+            onRequestClose={toggleModal}
+            style={customStyles}
+            contentLabel="My dialog"
+          >
+            <div>
+              <div className={classes.root}>
+                <Paper position="static">
+                  <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                    centered
+                    aria-label="simple tabs example"
+                  >
+                    <Tab label="Login" {...a11yProps(0)} />
+                    <Tab label="Register" {...a11yProps(1)} />
+                  </Tabs>
+                </Paper>
+
+                <TabPanel value={value} index={0}>
+                  {/* <ValidatorForm className="login-form" onSubmit={loginHandler}> */}
+
+                  <FormControl>
+                    <InputLabel htmlFor="username">Username*</InputLabel>
+                    <Input
+                      id="username"
+                      aria-describedby="my-helper-text"
+                      required
+                    />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+
+                  <br />
+                  <br />
+                  <FormControl>
+                    <InputLabel htmlFor="password">Password*</InputLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      required
+                      aria-describedby="my-helper-text"
+                    />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+
+                  <br />
+                  <br />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={loginHandler}
+                  >
+                    Login
+                  </Button>
+                  {/* </ValidatorForm> */}
+                </TabPanel>
+
+                <TabPanel value={value} index={1}>
+                  <FormControl>
+                    <InputLabel htmlFor="firstname">Firstname*</InputLabel>
+                    <Input id="firstname" aria-describedby="my-helper-text" />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+                  <br />
+                  <br />
+                  <FormControl>
+                    <InputLabel htmlFor="lastname">Lastname*</InputLabel>
+                    <Input id="lastname" aria-describedby="my-helper-text" />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+                  <br />
+                  <br />
+                  <FormControl>
+                    <InputLabel htmlFor="email">Email*</InputLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      aria-describedby="my-helper-text"
+                    />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+                  <br />
+                  <br />
+                  <FormControl>
+                    <InputLabel htmlFor="password">Password*</InputLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      aria-describedby="my-helper-text"
+                    />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+                  <br />
+                  <br />
+                  <FormControl>
+                    <InputLabel htmlFor="contact">Contact No.*</InputLabel>
+                    <Input
+                      id="contact"
+                      type="phone"
+                      aria-describedby="my-helper-text"
+                    />
+                    {/* <FormHelperText id="username-helper-text">
+                        We'll never share your email.
+                      </FormHelperText> */}
+                  </FormControl>
+                  <br />
+                  <br />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={registerHandler}
+                  >
+                    Register
+                  </Button>
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                  Item Three
+                </TabPanel>
+              </div>
+            </div>
+          </Modal>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
---
+++ src/screens/details/Details.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default function Details() {
+  return (
+    <Fragment>
+      <div>Details</div>
+    </Fragment>
+  );
+}
